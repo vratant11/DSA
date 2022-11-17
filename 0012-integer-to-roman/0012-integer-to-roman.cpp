7883f9d8237fe01c3d5d9b9e0c320c10d39921be
Vratant Singh <vratant2012061@akgec.ac.in>
2022-11-17 22:19:00 +0530

    Time: 20 ms (38.83%), Space: 8.8 MB (26.50%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string rom;
+        vector<string> notation = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        vector<int> value ={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        for(int i=0;num>0;++i){
+            while(num>= value[i]){
+                rom+=notation[i];
+                num -= value[i];
+            }
+        }
+        return rom;
+    }
+};
