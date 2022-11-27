23446bd629b4ac441a9b5adaec8ff5d732a47ee6
Vratant Singh <vratant2012061@akgec.ac.in>
2022-11-27 22:24:00 +0530

    Time: 8 ms (37.76%), Space: 8.3 MB (20.29%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+         map<int,int>m;
+        set<int>st;
+        for(int i=0;i<(arr.size());i++){
+	        m[arr[i]]++;
+	    }
+        for( auto i : m)
+        {
+            st.insert(i.second);
+        }
+        if(m.size()==st.size())
+       {
+           return true;
+       }
+        return false;
+        
+    }
+};
