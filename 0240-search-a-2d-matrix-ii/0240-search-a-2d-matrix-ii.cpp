71d545328deb8fcb9967cef59a6144ee62c7dc45
Vratant Singh <vratant2012061@akgec.ac.in>
2022-12-09 10:04:49 +0530

    Time: 515 ms (11.53%), Space: 14.9 MB (23.64%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& arr, int t) {
+        int m = arr.size();
+        int n = arr[0].size();
+        for(int row = 0;row<m;row++){
+            // binary search
+            int i = 0,j = n-1;
+            long int mid;
+            if(t<=arr[row][n-1]){
+                 while(i<=j){
+                mid =  (j+i)/2;
+                if(arr[row][mid] == t) return true;
+                if(arr[row][mid] > t) j = mid - 1;
+                else i = mid + 1;
+            }
+            }
+           
+           
+        }
+        return false;
+    }
+};
