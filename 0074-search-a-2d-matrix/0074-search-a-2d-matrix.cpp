136e15b76741006ab2f3250a2f6bd39e0fa1efed
Vratant Singh <vratant2012061@akgec.ac.in>
2022-12-09 09:59:03 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (78.73%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,23 +1,24 @@
 // class Solution {
 // public:
-//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-//         int row=matrix.size();
-//         int col=matrix[0].size();
-//         int startrow=0;
-//         int startcol=0;
-//         int endrow=row-1;
-//         int endcol=col-1;
-//         int total element=row*col;
-//         for(int i=0;int<row;i++){
-//             if(target<=matrix[startrow][endcol]){
-//                 int mid=martix[(startrow+endcol)/2];
-//                 if(target==mid) return true;
-//                 else if(target<mid) 
+//     bool searchMatrix(vector<vector<int>>& arr, int t) {
+//         int m = arr.size();
+//         int n = arr[0].size();
+//         for(int row = 0;row<m;row++){
+//             // binary search
+//             int i = 0,j = n-1;
+//             long int mid;
+//             if(t<=arr[row][n-1]){
+//                  while(i<=j){
+//                 mid =  (j+i)/2;
+//                 if(arr[row][mid] == t) return true;
+//                 if(arr[row][mid] > t) j = mid - 1;
+//                 else i = mid + 1;
 //             }
-//             else{
-//                 startrow++;
 //             }
+           
+           
 //         }
+//         return false;
 //     }
 // };
 
@@ -31,14 +32,14 @@ public:
             // binary search
             int i = 0,j = n-1;
             long int mid;
-            if(t<=arr[row][n-1]){
+            
                  while(i<=j){
                 mid =  (j+i)/2;
                 if(arr[row][mid] == t) return true;
                 if(arr[row][mid] > t) j = mid - 1;
                 else i = mid + 1;
             }
-            }
+            
            
            
         }
