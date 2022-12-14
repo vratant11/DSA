efefc92c42214b96175c163b520de23e1b8eeedf
Vratant Singh <vratant2012061@akgec.ac.in>
2022-12-14 22:57:53 +0530

    Time: 6 ms (83.67%), Space: 11.6 MB (85.30%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++) cout<<nums[i];
+        
         int n=nums.size();
         vector<int>v;
        
