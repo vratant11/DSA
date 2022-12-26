b1cb5522d711e689a35735ba6527b75b1c27d191
Vratant Singh <vratant2012061@akgec.ac.in>
2022-12-26 23:05:21 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (88.82%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sum = 0;
+        for (auto &c: t) sum+=c;
+        for (auto &c: s) sum-=c;
+        return (char)sum;
+    }
+};
+
