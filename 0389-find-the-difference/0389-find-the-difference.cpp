0e26354872a472588008e2dc72ca063b450ed11a
Vratant Singh <vratant2012061@akgec.ac.in>
2022-12-26 23:25:36 +0530

    Time: 19 ms (7.02%), Space: 6.6 MB (88.82%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,10 +1,24 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int sum = 0;
-        for (auto &c: t) sum+=c;
-        for (auto &c: s) sum-=c;
+       int sum1=0;
+        int sum2=0;
+        // vector<int> v(26, 0);
+        for(int i=0;i<t.length();i++){
+            if(i<s.length()){
+                int c=(int)s[i];
+                sum1=sum1+c;
+                cout<<sum1;
+            }
+            int c=(int)t[i];
+            sum2=sum2+c;
+        }
+       int sum=sum2-sum1;
+        cout<<sum;
         return (char)sum;
     }
 };
+
+
+
 
