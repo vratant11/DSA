49634138d84d129eee2dfc5d78903abb5a6ea679
Vratant Singh <vratant2012061@akgec.ac.in>
2022-12-27 18:03:50 +0530

    Time: 134 ms (64.01%), Space: 16.8 MB (7.58%) - LeetHub

---
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        int sum=0;
+        map<char,int>m;
+        for(int i=0;i<s.length();i++){
+            m[s[i]]++;
+        }
+        for(int i=0;i<t.length();i++){
+            m[t[i]]--;
+        }
+        for(auto i:m){
+            if(i.second<0)
+                sum += i.second;
+        }
+        return abs(sum);
+    }
+};
