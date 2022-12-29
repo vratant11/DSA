43abc6185757a4e834a46755bff504ff5048a8a2
Vratant Singh <vratant2012061@akgec.ac.in>
2022-12-29 21:09:09 +0530

    Time: 7 ms (98.92%), Space: 12.6 MB (83.22%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        vector<vector<int>>ans;
+        for(int i=1;i<=*max_element(groupSizes.begin(),groupSizes.end());i++){
+            vector<int>sub;
+            for(int j=0;j<groupSizes.size();j++){
+                if(groupSizes[j]==i){
+                    if(sub.size()>=i){
+                        ans.push_back(sub);
+                        sub.clear();
+                    }
+                    sub.push_back(j);
+                }
+            }
+            if(sub.size()){
+                ans.push_back(sub);
+            }
+        }
+        return ans;
+    }
+};
