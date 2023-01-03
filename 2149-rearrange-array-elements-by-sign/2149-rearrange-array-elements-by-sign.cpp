35d06e535ed426ffae738316ff7bb2a312a609be
Vratant Singh <vratant2012061@akgec.ac.in>
2023-01-03 17:23:42 +0530

    Time: 237 ms (85.70%), Space: 123.7 MB (95.11%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
    vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> ans(nums.size());
+        vector<int> ans(nums.size(),0);
         int indexpos = 0, indexneg=1;
         for(auto num: nums){
             if(num>0){
