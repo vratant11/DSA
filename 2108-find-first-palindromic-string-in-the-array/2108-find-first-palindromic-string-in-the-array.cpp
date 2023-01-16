867949c15a50f0baac14b2f19b1961cc8c04e39c
Vratant Singh <vratant2012061@akgec.ac.in>
2023-01-16 22:11:51 +0530

    Time: 60 ms (81.45%), Space: 23 MB (42.12%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
@@ -1,11 +1,11 @@
 bool isPalindrome(string str){
         int left=0 ;
-        int rigt=str.size()-1;
-        while( left<= rigt ){
-            if( str[left] != str[rigt] )
+        int right=str.size()-1;
+        while( left<= right ){
+            if( str[left] != str[right] )
                 return false;
             left++;
-            rigt--;
+            right--;
         }
         return true;
     }
