dcfa019e5919ebb3621517982557f00acb612fb5
Vratant Singh <vratant2012061@akgec.ac.in>
2023-01-20 21:33:56 +0530

    Time: 12 ms (55.95%), Space: 10.3 MB (9.13%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+         map<int,int>mp;
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        
+        
+        for (auto i : mp){
+            if(i.second==1){
+                ans=i.first;
+            }
+        }
+        return ans;
+    }
+};
