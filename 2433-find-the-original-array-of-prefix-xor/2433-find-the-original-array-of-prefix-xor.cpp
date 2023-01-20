55c00abe107db4aa119c2a35635ace1db4b667dd
Vratant Singh <vratant2012061@akgec.ac.in>
2023-01-20 21:26:56 +0530

    Time: 142 ms (52.27%), Space: 79.3 MB (22.13%) - LeetHub

---
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        vector<int>ans;
+        ans.push_back(pref[0]);
+        int total=pref[0];
+        int v=0;
+        for(int i=1;i<pref.size();i++){
+           
+            ans.push_back(pref[i-1]^pref[i]);
+            
+        }
+        return ans;
+    }
+};
