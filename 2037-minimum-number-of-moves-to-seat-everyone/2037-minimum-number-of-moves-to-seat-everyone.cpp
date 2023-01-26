84a37f63ce18f97fb69d7315b0b8ee5b23e76a08
Vratant Singh <vratant2012061@akgec.ac.in>
2023-01-26 20:43:21 +0530

    Time: 25 ms (8.68%), Space: 18.2 MB (51.26%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
@@ -3,10 +3,10 @@ public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
         sort(seats.begin(),seats.end());
         sort(students.begin(),students.end());
-        int count =0;
+        int counts =0;
         for(int i=0;i<seats.size();i++){
-            count +=abs(seats[i]-students[i]);
+            counts +=abs(seats[i]-students[i]);
         }
-        return count;
+        return counts;
     }
 };
