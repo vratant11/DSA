ed7ae3c21996a2a2300559fb65a5b20456829a44
Vratant Singh <vratant2012061@akgec.ac.in>
2023-01-29 23:05:59 +0530

    Time: 59 ms (74.54%), Space: 28.3 MB (8.99%) - LeetHub

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -1,31 +1,31 @@
-// class Solution {
-// public:
-//     int arrayPairSum(vector<int>& nums) {
-//         sort(nums.begin(),nums.end());
-//         int n=nums.size();
-//         int sum=0;
-//         for(int i=1;i<n;i=i+2){
-//             sum+=min(nums[i-1],nums[i]);
-//         }
-//         return sum;
-//     }
-// };
-
-
 class Solution {
 public:
-    int arrayPairSum(vector<int>& nums)
-    {
+    int arrayPairSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        int ans=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(i%2==0)
-            {
-                ans = ans + nums[i];
-            }
+        int n=nums.size();
+        int sum=0;
+        for(int i=1;i<n;i=i+2){
+            sum+=min(nums[i-1],nums[i]);
         }
-        return ans;
+        return sum;
     }
 };
+
+
+// class Solution {
+// public:
+//     int arrayPairSum(vector<int>& nums)
+//     {
+//         sort(nums.begin(),nums.end());
+//         int ans=0;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(i%2==0)
+//             {
+//                 ans = ans + nums[i];
+//             }
+//         }
+//         return ans;
+//     }
+// };
 
