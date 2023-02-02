b57d904df28b711ce90784787c85cb04605be6b5
Vratant Singh <vratant2012061@akgec.ac.in>
2023-02-02 21:51:52 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (81.07%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -1,14 +1,26 @@
-class Solution {
-public:
+// class Solution {
+// public:
+//     int numJewelsInStones(string jewels, string stones) {
+//         int c=0;
+//         for(int i=0;i<jewels.length();i++){
+//             for(int j=0;j<stones.length();j++){
+//                 if(jewels[i]==stones[j]){
+//                     c++;
+//                 }
+//             }
+//         }
+//         return c;
+//     }
+// };
+
+
+class Solution{
+    public:
     int numJewelsInStones(string jewels, string stones) {
-        int c=0;
-        for(int i=0;i<jewels.length();i++){
-            for(int j=0;j<stones.length();j++){
-                if(jewels[i]==stones[j]){
-                    c++;
-                }
-            }
-        }
-        return c;
+    int num = 0;
+    for (int i = 0; i < jewels.size(); i++){
+        num += count(stones.begin(), stones.end(), jewels[i]);
     }
+    return num;
+}
 };
