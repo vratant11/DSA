d61be8cddff42da35ff148008a62ae61affccb07
Vratant Singh <vratant2012061@akgec.ac.in>
2023-02-14 12:27:27 +0530

    Time: 231 ms (83.22%), Space: 114.1 MB (85.39%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,21 +10,43 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        vector<int>arr;
-        while(head){
-            arr.push_back(head->val);
-            head = head->next;
+    ListNode* middle(ListNode* head){
+        ListNode* sp = head;
+        ListNode* fp = head;
+        while(fp && fp->next){
+            sp = sp->next;
+            fp = fp->next->next;
         }
-        int s = 0;
-        int e = arr.size()-1;
-        while(s<e){
-            if(arr[s]!=arr[e]){
+        return sp;
+    }
+    ListNode* reverse(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next = head;
+        while(curr){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    
+    bool compare(ListNode* l1, ListNode* l2){
+        while(l1 && l2){
+            if(l1->val != l2->val){
                 return false;
             }
-            s++;
-            e--;
+            l1 = l1->next;
+            l2 = l2->next;
         }
         return true;
+    }
+    
+    bool isPalindrome(ListNode* head) {
+        
+        ListNode* mid = middle(head);
+        ListNode* rev = reverse(mid);
+        return compare(head, rev);        
     }
 };
