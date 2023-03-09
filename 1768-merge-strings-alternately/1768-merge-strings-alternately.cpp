076170c19b4982d3e3f2b56d0fdbdd9cd5c25479
Vratant Singh <vratant2012061@akgec.ac.in>
2023-03-09 20:15:42 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (83.79%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,27 +1,51 @@
+// class Solution {
+// public:
+//     string mergeAlternately(string word1, string word2) {
+        
+//         string str;
+
+//         int i = 0, s1 = word1.length(), s2 = word2.length();
+
+//         while(i < s1 && i < s2){
+//             str.push_back(word1[i]);
+//             str.push_back(word2[i]);
+//             i++;
+//         }
+
+//         while(i < s1){
+//             str.push_back(word1[i]);
+//             i++;
+//         }
+        
+//         while(i < s2){
+//             str.push_back(word2[i]);
+//             i++;
+//         }
+
+//         return str;
+//     }
+// };
+
+
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        
-        string str;
+        string s;
 
-        int i = 0, s1 = word1.length(), s2 = word2.length();
-
-        while(i < s1 && i < s2){
-            str.push_back(word1[i]);
-            str.push_back(word2[i]);
-            i++;
+     
+        int i{0}, j{0};
+        while (i<word1.length() && j<word2.length()) {
+            s+=word1[i++];
+            s+=word2[j++];
         }
 
-        while(i < s1){
-            str.push_back(word1[i]);
-            i++;
-        }
-        
-        while(i < s2){
-            str.push_back(word2[i]);
-            i++;
+       
+        if (word1.length() > word2.length()) {
+            s.append(word1, i);
+        } else if (word1.length() < word2.length()) {
+            s.append(word2, j);
         }
 
-        return str;
+        return s;
     }
 };
