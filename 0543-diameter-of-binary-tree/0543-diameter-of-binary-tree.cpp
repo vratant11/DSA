97b58fd6a94e3287ffd0c34d2e1eabb76263e9f0
Vratant Singh <vratant2012061@akgec.ac.in>
2023-03-24 23:44:31 +0530

    Time: 3 ms (98.88%), Space: 20.9 MB (8.91%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -9,19 +9,46 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int mx =0 ; 
+//     int diameter_bst(TreeNode* temp){
+//         if(temp == nullptr )
+//         return 0 ; 
+//         int l =  diameter_bst(temp->left) ; 
+//         int r  = diameter_bst(temp->right) ;
+//         mx = max(mx , l+r) ; 
+//         return (1+max(l , r)) ; 
+//     }
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         diameter_bst(root) ; 
+//          return mx ; 
+//     }
+// };
+ 
+
+
+
 class Solution {
 public:
-    int mx =0 ; 
-    int diameter_bst(TreeNode* temp){
-        if(temp == nullptr )
-        return 0 ; 
-        int l =  diameter_bst(temp->left) ; 
-        int r  = diameter_bst(temp->right) ;
-        mx = max(mx , l+r) ; 
-        return (1+max(l , r)) ; 
+    pair<int,int> solve(TreeNode* root){
+        //base case
+        if(root==NULL)return {0,0};
+        
+        pair<int,int> left = solve(root->left);
+        pair<int,int> right = solve(root->right);
+        
+        int notpassing = max(left.first,right.first)+1;
+        
+        int passing = max({ 
+            left.second,right.second,
+            left.first+right.first+1,
+            });
+        return {notpassing,passing};
     }
+        
     int diameterOfBinaryTree(TreeNode* root) {
-        diameter_bst(root) ; 
-         return mx ; 
+        pair<int,int>ans = solve(root);
+        return max(ans.first,ans.second)-1;
     }
 };
