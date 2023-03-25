890887a2be0437831fde19a09cbc5d45ed824f6a
Vratant Singh <vratant2012061@akgec.ac.in>
2023-03-25 15:38:07 +0530

    Time: 11 ms (82.38%), Space: 22.1 MB (6.24%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -14,32 +14,31 @@ class Solution {
     
 public:
     bool isBalanced(TreeNode* root) {
-        if(root==NULL){
-            return true;
-        }
-        bool left =isBalanced(root->left);
-        bool right =isBalanced(root->right);
-        
-        bool diff=abs(Height(root->left)-Height(root->right)) <=1;
-        
-        if(left&&right&&diff){
-            return true;
-        }
-        else{
-            return false;
-        }
+       return isBalancedFast(root).first;
     }
     
-     int Height(TreeNode* root) {
-       
-		if (root == NULL)  return 0;
-    
-		int leftHeight = Height(root->left);
+    pair<bool,int> isBalancedFast(TreeNode*root){
+         if(root==NULL){
+             pair<bool,int>p=make_pair(true,0);
+            return p;
+        }
+        pair<bool,int> left =isBalancedFast(root->left);
+        pair<bool,int> right =isBalancedFast(root->right);
         
-		int rightHight = Height(root->right);
+        bool leftAns=left.first;
+        bool rightAns=right.first;
         
-		
         
-		return max(leftHeight, rightHight) + 1;
+        
+        bool diff=abs(left.second-right.second)<=1;
+        pair<bool,int>ans;
+        ans.second = max(left.second,right.second)+1;
+        if(leftAns&&rightAns&&diff){
+            ans.first=true;
+        }
+        else{
+            ans.first=false;
+        }
+        return ans;
     }
 };
